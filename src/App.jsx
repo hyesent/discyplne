a2d0f98e91e950436dc0e430ddf4d5060e1d4b05
hyesent <hyacinthmichael36@gmail.com>
2026-06-02 22:07:49 +0100

    Fix destructuring assignment for subscription

--- src/App.jsx
+++ src/App.jsx
@@ -159,7 +159,7 @@ export default function App() {
         fetchPomodoroStats()
       }
     })
-    const { data: { subscription } = supabase.auth.onAuthStateChange((_event, session) => {
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session)
       setUser(session?.user?? null)
       if (session?.user) {
